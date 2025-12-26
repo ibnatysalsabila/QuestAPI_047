f535522e2961d936a3aedbae94308d99ff6b2ef8
ibnatysalsabila <salsajpr01@gmail.com>
2025-12-27 00:10:04 +0700

    menambahkan DetailViewModel dan logika pengambilan satu data siswa

--- app/src/main/java/com/example/questapi_047/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_047/viewmodel/DetailViewModel.kt
@@ -20,3 +20,34 @@ sealed interface StatusUiDetail {
     object Error : StatusUiDetail
     object Loading : StatusUiDetail
 }
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoryDataSiswa
+) : ViewModel() {
+
+    private val idSiswa: Int =
+        checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+    var statusUiDetail: StatusUiDetail by mutableStateOf(StatusUiDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
+
+    fun getSatuSiswa() {
+        viewModelScope.launch {
+            statusUiDetail = StatusUiDetail.Loading
+            statusUiDetail = try {
+                StatusUiDetail.Success(
+                    satusiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                )
+            } catch (e: IOException) {
+                StatusUiDetail.Error
+            } catch (e: HttpException) {
+                StatusUiDetail.Error
+            }
+        }
+    }
+
