b44cd5d756d74ed958e0e0790bd6c5fb92d7b0c7
ibnatysalsabila <salsajpr01@gmail.com>
2025-12-27 00:12:29 +0700

    inisialisasi uiStateSiswa dan mengambil data siswa berdasarkan ID

--- app/src/main/java/com/example/questapi_047/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_047/viewmodel/EditViewModel.kt
@@ -19,3 +19,16 @@ class EditViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryDataSiswa: RepositoryDataSiswa
 ) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
+
