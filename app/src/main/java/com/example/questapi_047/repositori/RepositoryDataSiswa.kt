28e29303984142fec0d42357473686ebfdc6e96f
ibnatysalsabila <salsajpr01@gmail.com>
2025-12-26 23:50:23 +0700

    menambahkan deklarasi fungsi get, edit, dan hapus satu siswa pada repository interface

--- app/src/main/java/com/example/questapi_047/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_047/repositori/RepositoryDataSiswa.kt
@@ -4,9 +4,13 @@ import com.example.questapi_047.apiservice.ServiceApiSiswa
 import com.example.questapi_047.modeldata.DataSiswa
 
 interface RepositoryDataSiswa{
-    suspend fun getDataSiswa() : List<DataSiswa>
+    suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id: Int): DataSiswa
+    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa):retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id: Int):retrofit2.Response<Void>
 }
+
 class JaringanRepositoryDataSiswa(
     private val serviceApiSiswa: ServiceApiSiswa
 ):RepositoryDataSiswa{
